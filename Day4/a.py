b645e318c1fd5d22ad4afb739f2221e51dffa834
CHAEUN PARK <codjs0910@naver.com>
2019-07-04 13:06:02 +0900

    json

---
+++ Day4/a.py
@@ -0,0 +1,11 @@
+import json
+
+student = {
+    'id': 2002324,
+    'name': '홍길동',
+    'history': [
+        {'date': '2018-03-11', 'lang': 'java'},
+        {'date': '2018-07-23', 'lang': 'python'},
+    ]
+}
+# JSON making jsonString = json.dumps(student, ensure_ascii=False, indent=4) print(jsonString) print(type(jsonString))
